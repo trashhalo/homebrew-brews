2613c52e02d87ebc62d856658d37493fb42e1b11
goreleaserbot <goreleaser@carlosbecker.com>
2020-10-27 09:19:03 -0400

    Brew formula update for imgcat version v1.1.1

--- Formula/imgcat.rb
+++ Formula/imgcat.rb
@@ -2,16 +2,16 @@
 class Imgcat < Formula
   desc "Tool to output images in the terminal"
   homepage "https://github.com/trashhalo/imgcat"
-  version "1.1.0"
+  version "1.1.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/trashhalo/imgcat/releases/download/v1.1.0/imgcat_1.1.0_Darwin_x86_64.tar.gz"
-    sha256 "c6c0de4d403ef3c83a13ba04867c11ba8ad8109342ee72e09b6411e0d469f946"
+    url "https://github.com/trashhalo/imgcat/releases/download/v1.1.1/imgcat_1.1.1_Darwin_x86_64.tar.gz"
+    sha256 "541d6a69532631a3c363391af8af130c224f478438324cd1a6c3f7ba84b901c3"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/trashhalo/imgcat/releases/download/v1.1.0/imgcat_1.1.0_Linux_x86_64.tar.gz"
-      sha256 "67e63cbbe5f0600a23d1db4569766247283330db9a707797e1f67d3e048c5b07"
+      url "https://github.com/trashhalo/imgcat/releases/download/v1.1.1/imgcat_1.1.1_Linux_x86_64.tar.gz"
+      sha256 "2d4d170a4a2708343afbaff12224482edfd0b91be1b5c48c281dc1148b12972e"
     end
   end
 
